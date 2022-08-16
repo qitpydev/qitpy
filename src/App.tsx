67e6f45382ab4cd72af0c8d567c5dcb00707d9cb
Quyet <quyet.doan@smartdev.com>
2022-08-16 20:58:38 +0700

    add redux react-router6

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,76 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Welcome to React</h1>
+      <Routes>
+        <Route path="" element={<div><Home /><Outlet /></div>}>
+          <Route index element={<h1>Index hhhh</h1>} />
+          <Route path="teams" element={<h1>Teams<Outlet /></h1>}>
+            <Route path=":teamId" element={<h1>:teamId</h1>} />
+            <Route path="new" element={<h1>New</h1>} />
+            <Route index element={<h1>Nested Index</h1>} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Route>
+      </Routes>
+      <h1>Ahhhhhhh</h1>
+
+      <h1>Welcome to React2</h1>
+      <Routes>
+        <Route path="" element={<div><About /><Outlet /></div>}>
+          <Route index element={<h1>Index hhhh</h1>} />
+          <Route path="teams" element={<h1>Teams<Outlet /></h1>}>
+            <Route path=":teamId" element={<h1>:teamId</h1>} />
+            <Route path="new" element={<h1>New</h1>} />
+            <Route index element={<h1>Nested Index</h1>} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Route>
+      </Routes>
+      <h1>Ahhhhhhh</h1>
     </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      404 NOT FOUND
+    </div>
+  )
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      {/* <nav> */}
+        <Link to="/">Home</Link> |{" "}
+        <Link to="about">About</Link>
+      {/* </nav> */}
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import { BrowserRouter } from 'react-router-dom'
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
