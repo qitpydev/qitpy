a1ac4b33f8adaa0da87849bb974dbb098f4fe7d1
vanquyet <code.maito@gmail.com>
2022-09-03 21:31:31 +0700

    portfolio working on scroll-wrapper-top

--- src/components/Portfolio/PortfolioContent/PortfolioContent.jsx
+++ src/components/Portfolio/PortfolioContent/PortfolioContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './PortfolioContent.scss'
 
 function PortfolioContent() {
   return (
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import PortfolioHeader from '../../components/Portfolio/PortfolioHeader/PortfolioHeader';
 import PortfolioContent from '../../components/Portfolio/PortfolioContent/PortfolioContent';
 import PortfolioFooter from '../../components/Portfolio/PortfolioFooter/PortfolioFooter';
+import './Portfolio.scss'
+import { BsChevronDoubleDown } from 'react-icons/bs';
 
 function Portfolio() {
 
-  const [onFooter, setOnFooter] = useState(false)
+  const [showFooterOn, setShowFooterOn] = useState(false)
 
-  const handleOnScroll = () => {
-    
+  const handleOnScroll = (event) => {
+    if (event.deltaY > 0) {
+      setShowFooterOn(true)
+    } else if (event.deltaY < 0) {
+      setShowFooterOn(false)
+    }
   }
 
   return (
-    <div className='portfolio' onScroll={handleOnScroll}>
-      <PortfolioHeader />
-      <PortfolioContent />
-      <PortfolioFooter />
+    <div className='portfolio' onWheel={handleOnScroll}>
+      { !showFooterOn && <PortfolioHeader   /> }
+      { !showFooterOn && <PortfolioContent  /> }
+      {  showFooterOn && <PortfolioFooter   /> }
+      <BsChevronDoubleDown className='portfolio-scroll-wrapper'/>
+      <BsChevronDoubleDown className='portfolio-scroll-wrapper-2'/>
     </div>
   )
 }
