e79b2c006701fb888ecfe1cfad09167069a5e33a
vanquyet <code.maito@gmail.com>
2022-09-06 18:38:19 +0700

    fuckingg life

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_learn',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- backend/services/profiles_api/views.py
+++ backend/services/profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class ProfileApiView(APIView):
+    """Test API VIEW"""
+    
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, put, delete)',
+            'Is similar the most control over you application logic',
+            'Gives you the most control over you application logic'
+            'Is mapped manually to URLs'
+        ]
+        
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
