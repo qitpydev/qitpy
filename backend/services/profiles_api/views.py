a5b8dddb7b46f593061a426b386c821f49ba5851
vanquyet <code.maito@gmail.com>
2022-09-10 14:27:39 +0700

    ViewSet update

--- backend/services/profiles_api/views.py
+++ backend/services/profiles_api/views.py
@@ -52,6 +52,8 @@ class ProfileApiView(APIView):
 class ProfileViewSet(viewsets.ViewSet):
     """Test API Viewsets"""
     
+    serializers_class = serializers.ProfileSerializer
+    
     def list(self, request):
         """Return """
         
@@ -62,5 +64,36 @@ class ProfileViewSet(viewsets.ViewSet):
         ]
         
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+    
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializers_class(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle update an object"""
+        
+        return Response({'http_method':'PUT'})
+    
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method':'PATCH'})
+    
+    def destroy(self, request, pk=None):
+        """Handle deleting an object"""
+        return Response({'http_method':'DELETE'})
         
         
