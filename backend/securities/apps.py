f0d997ebcb41fedde11ec944b77f72d3f7b24d45
quyet <code.maito@gmail.com>
2022-10-12 22:31:11 +0700

    refactor/remove-server

--- backend/securities/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SecuritiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'securities'
--- backend/services/profiles_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from services.profiles_api import models
-# Register your models here.
-
-admin.site.register(models.UserProfile)
-admin.site.register(models.ProfileFeedItem)
