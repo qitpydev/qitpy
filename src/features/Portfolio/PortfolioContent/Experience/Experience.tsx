b9dd47f9fed5af393f1c57999d8ae2ffa79ee8e7
quyet <code.maito@gmail.com>
2022-10-14 16:34:28 +0700

    delete project card animation, update year exp

--- src/features/Portfolio/PortfolioContent/Experience/Experience.tsx
+++ src/features/Portfolio/PortfolioContent/Experience/Experience.tsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './Experience.scss'
 import ExperienceTimeline from '../../components/ExperienceTimeline/ExperienceTimeline'
+import { getWorkingTimes } from '../../../../common/function/functions';
 
 function Experience(props) {
   return (
     <div className='portfolio-experience'>
       <div className="label-rotate">Experiences</div>
       <div className="work-experience-years">
-            ~ 2 years experience
+            {getWorkingTimes()}
       </div>
       <div className="work-experience-container">
         <ExperienceTimeline />
