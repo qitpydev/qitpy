461bd975310189e9d44b5abf6875807e170f36d5
quyet <code.maito@gmail.com>
2022-11-16 23:25:09 +0700

    change logo & name

--- src/features/PortfolioHeader/PortfolioHeader.tsx
+++ src/features/PortfolioHeader/PortfolioHeader.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './PortfolioHeader.scss'
 import PortfolioHeaderQuote from './PortfolioHeaderQuote/PortfolioHeaderQuote';
 import PortfolioContact from './PortfolioContact/PortfolioContact';
+import logo from '../../images/favicon.png';
 
 function PortfolioHeader() {
   return (
     <div className='portfolio-header'>
       <div className='portfolio-header-logo'>
-          LOGO
+          <img src={logo}/>
       </div>
       <div className="portfolio-header-quote">
         <PortfolioHeaderQuote />
