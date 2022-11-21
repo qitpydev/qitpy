62b29a12e91e75d8a34552795a9d405f266043b5
quyet <code.maito@gmail.com>
2022-11-21 17:03:01 +0700

    update padding of trip project_cards to lower

--- src/features/components/AppWorking/Util.ts
+++ src/features/components/AppWorking/Util.ts
@@ -3,7 +3,7 @@ import { ElementId } from "../../../common/function/functions";
 const setDefaultValueForScrollPosition = () => {
     const appWorkingCardContainerElement = document.getElementById(ElementId.AppWorkingCardContainer)
     if (appWorkingCardContainerElement) {
-        appWorkingCardContainerElement.scrollLeft += 50;
+        appWorkingCardContainerElement.scrollLeft += 30;
     }
 }
 export {setDefaultValueForScrollPosition}
