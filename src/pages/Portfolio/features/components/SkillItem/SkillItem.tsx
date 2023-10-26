418ae213b0a10e6ac62c7b23d6bd2fc2fbddedb8
quyet <qitpydev@gmail.com>
2023-10-26 09:49:08 +0700

    fix: footer wrap text

--- src/pages/Portfolio/features/components/SkillItem/SkillItem.tsx
+++ src/pages/Portfolio/features/components/SkillItem/SkillItem.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './SkillItem.scss'
 import { SkillDetail } from '../../../../../data'
-// import {SkillDetail} from '../../../data'
+import MediaQuery from 'react-responsive';
 
 SkillItem.defaultProps = {
   id: "id",
