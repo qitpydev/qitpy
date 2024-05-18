08c39c30e75c6f000d4a4136b76fc2f73b9d017b
quyet <code.maito@gmail.com>
2024-05-18 20:21:46 +0700

    clean

--- src/pages/Portfolio/features/components/AppWorking/Util.ts
+++
@@ -1,9 +0,0 @@
-import { ElementId } from "../../../common/function/functions";
-
-const setDefaultValueForScrollPosition = (value) => {
-    const appWorkingCardContainerElement = document.getElementById(ElementId.AppWorkingCardContainer)
-    if (appWorkingCardContainerElement) {
-        appWorkingCardContainerElement.scrollLeft += value;
-    }
-}
-export {setDefaultValueForScrollPosition}
--- src/pages/Portfolio/features/components/CV/components/EducationCv/EducationCv.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function EducationCv() {
-  return (
-    <div>EducationCv</div>
-  )
-}
-
-export default EducationCv
